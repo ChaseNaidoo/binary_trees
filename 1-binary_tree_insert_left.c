4605b64cf6087b0ad4a0e5f239487489fcbb6d89
root <chasenaidoo9@gmail.com>
2023-11-01 06:41:59 -0700

    Added permissons

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -29,7 +29,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = parent->left;
 	new_node->left->parent = new_node;
 	}
-
 	parent->left = new_node;
 	return (new_node);
 }
