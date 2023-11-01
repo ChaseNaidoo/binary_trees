1b8faa2bb2459502b80909020047e69eedca4633
root <chasenaidoo9@gmail.com>
2023-11-01 09:50:33 -0700

    Count at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  * Return: Number of leaves in the binary tree, or 0 if tree is NULL
  *
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-	return (1);
+	if (tree->left || tree->right)
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (0);
 }
