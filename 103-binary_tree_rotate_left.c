07570464838389f4624d34e1584874dce6ab12f2
root <chasenaidoo9@gmail.com>
2023-11-02 07:31:51 -0700

    Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	binary_tree_t *new_root;
 
 	if (tree == NULL || tree->right == NULL)
-	  return (tree);
+	return (tree);
 
 	new_root = tree->right;
 	tree->right = new_root->left;
