631b25cc847fe43867f168b70a128cd49cecce2e
root <chasenaidoo9@gmail.com>
2023-11-02 07:29:55 -0700

    Rotate left

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -9,11 +9,6 @@
  * Return: (no return)
  *
  */
-/**
- * binary_tree_levelorder - Perform level-order traversal on a binary tree.
- * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
- */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
   int height = binary_tree_height(tree);
